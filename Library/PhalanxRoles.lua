ec43d87b085cfeefd70bf8917a215cddbe513075
johnathan clause <hashmalum@gmail.com>
2024-04-29 21:39:28 -0500

    fixed compatibility issues

--- Library/PhalanxRoles.lua
+++ Library/PhalanxRoles.lua
@@ -1,14 +1,16 @@
-local ROLES = require( GetScriptDirectory() .. '\\Library\\_PhalanxRoles' );
-function ROLES.GetPRole(bot, hero)
-    local UTILS = require( GetScriptDirectory() .. '\\cmai\\utils' );
-    local role = 
-    {
-        ['safe']    = 'SafeLane',
-        ['mid']     = 'MidLane',
-        ['off']     = 'OffLane',
-        ['soft']    = 'SoftSupport',
-        ['hard']    = 'HardSupport',
-    }
-    return role[UTILS.GetHeroRoles()[hero]]
+local ROLES = require( GetScriptDirectory() .. '\\Library\\_PhalanxRoles' )
+if GetGameMode() == GAMEMODE_CM then
+    function ROLES.GetPRole(bot, hero)
+        local UTILS = require( GetScriptDirectory() .. '\\cmai\\utils' )
+        local role = 
+        {
+            ['safe']    = 'SafeLane',
+            ['mid']     = 'MidLane',
+            ['off']     = 'OffLane',
+            ['soft']    = 'SoftSupport',
+            ['hard']    = 'HardSupport',
+        }
+        return role[UTILS.GetHeroRoles()[hero]]
+    end
 end
 return ROLES
