b1d51bee604e36a735758df87887d0699d3ad22c
johnathan clause <hashmalum@gmail.com>
2024-04-29 21:36:59 -0500

    fixed compatibility issues

--- RoleUtility.lua
+++ RoleUtility.lua
@@ -1,16 +1,16 @@
 local _ROLE = require( GetScriptDirectory() .. '\\_RoleUtility' );
-local _getCurrentSuitableRole = _ROLE.GetCurrentSuitableRole;
-function _ROLE.GetCurrentSuitableRole(bot, hero)
-    local UTIL = require( GetScriptDirectory() .. '\\cmai\\utils' );
-    local role = 
-    {
-        ['safe']    = 'carry',
-        ['mid']     = 'midlaner',
-        ['off']     = 'offlaner',
-        ['soft']    = 'support',
-        ['hard']    = 'support',
-    }
-    return GetGameMode() == GAMEMODE_CM and role[UTIL.GetHeroRoles()[hero]]
-        or _getCurrentSuitableRole(bot, hero)
+if GetGameMode() == GAMEMODE_CM then
+    function _ROLE.GetCurrentSuitableRole(bot, hero)
+        local UTIL = require( GetScriptDirectory() .. '\\cmai\\utils' );
+        local role = 
+        {
+            ['safe']    = 'carry',
+            ['mid']     = 'midlaner',
+            ['off']     = 'offlaner',
+            ['soft']    = 'support',
+            ['hard']    = 'support',
+        }
+        return role[UTIL.GetHeroRoles()[hero]]
+    end
 end
 return _ROLE
