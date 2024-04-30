6444d5dabaf531a9c14289be45a54d7ad3d31c3a
johnathan clause <hashmalum@gmail.com>
2024-04-29 21:41:59 -0500

    fixed compatibility issues

--- FunLib/aba_item.lua
+++ FunLib/aba_item.lua
@@ -1,14 +1,16 @@
-local _ITEM = require( GetScriptDirectory() .. '\\FunLib\\_aba_item' );
-function _ITEM.GetRoleItemsBuyList( bot )
-    local UTILS = require( GetScriptDirectory() .. '\\cmai\\utils' );
-    local role = 
-    {
-		['safe']    = 'pos_1',
-		['mid']     = 'pos_2',
-		['off']     = 'pos_3',
-		['soft']    = 'pos_4',
-		['hard']    = 'pos_5',
-	}
-	return role[UTILS.GetHeroRoles()[bot:GetUnitName()]] or 'pos_1'
+local ITEM = require( GetScriptDirectory() .. '\\FunLib\\_aba_item' )
+if GetGameMode() == GAMEMODE_CM then
+	function _ITEM.GetRoleItemsBuyList( bot )
+		local UTILS = require( GetScriptDirectory() .. '\\cmai\\utils' )
+		local role = 
+		{
+			['safe']    = 'pos_1',
+			['mid']     = 'pos_2',
+			['off']     = 'pos_3',
+			['soft']    = 'pos_4',
+			['hard']    = 'pos_5',
+		}
+		return role[UTILS.GetHeroRoles()[bot:GetUnitName()]] or 'pos_1'
+	end
 end
-return _ITEM
+return ITEM
--- FunLib/jmz_func.lua
+++ FunLib/jmz_func.lua
@@ -1,14 +1,16 @@
-local _JMZ = require( GetScriptDirectory() .. '\\FunLib\\_jmz_func' );
-function _JMZ.GetPosition( bot )
-    local UTILS = require( GetScriptDirectory() .. '\\cmai\\utils' );
-    local pos = 
-    {
-        ['safe'] = 1,
-        ['mid'] = 2,
-        ['off'] = 3,
-        ['soft'] = 4,
-        ['hard'] = 5,
-    }
-    return pos[UTILS.GetHeroRoles()[bot:GetUnitName()]] or 1
+local JMZ = require( GetScriptDirectory() .. '\\FunLib\\_jmz_func' )
+if GetGameMode() == GAMEMODE_CM then
+    function _JMZ.GetPosition( bot )
+        local UTILS = require( GetScriptDirectory() .. '\\cmai\\utils' )
+        local pos = 
+        {
+            ['safe'] = 1,
+            ['mid'] = 2,
+            ['off'] = 3,
+            ['soft'] = 4,
+            ['hard'] = 5,
+        }
+        return pos[UTILS.GetHeroRoles()[bot:GetUnitName()]] or 1
+    end
 end
-return _JMZ
+return JMZ
