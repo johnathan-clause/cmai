f5a79d57492ddedc396357b1392f81b07d8035e9
johnathan clause <hashmalum@gmail.com>
2024-04-29 20:05:28 -0500

    compatibility for bot script

---
+++ RoleUtility.lua
@@ -0,0 +1,14 @@
+local _ROLE = require( GetScriptDirectory() .. '\\_RoleUtility' );
+function _ROLE.GetCurrentSuitableRole(bot, hero)
+    local UTIL = require( GetScriptDirectory() .. '\\cmai\\utils' );
+    local role = 
+    {
+        ['safe']    = 'carry',
+        ['mid']     = 'midlaner',
+        ['off']     = 'offlaner',
+        ['soft']    = 'support',
+        ['hard']    = 'support',
+    }
+    return role[UTIL.GetHeroRoles()[hero]]
+end
+return _ROLE
