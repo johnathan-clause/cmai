f9bb3e4a3ea6fd62b0c10684f27b088dd30984f5
johnathan clause <hashmalum@gmail.com>
2024-04-30 17:28:23 -0500

    fixed typo

--- FunLib/aba_role.lua
+++ FunLib/aba_role.lua
@@ -4,11 +4,11 @@ if GetGameMode() == GAMEMODE_CM then
         local UTILS = require( GetScriptDirectory() .. '\\cmai\\utils' )
         local role =
         {
-            ['safe']    = 'carry'.
-            ['mid']     = 'midlaner'.
-            ['off']     = 'offlaner'.
-            ['soft']    = 'support'.
-            ['hard']    = 'support'.
+            ['safe']    = 'carry',
+            ['mid']     = 'midlaner',
+            ['off']     = 'offlaner',
+            ['soft']    = 'support',
+            ['hard']    = 'support',
         }
         return role[UTILS.GetHeroRoles()[hero]]
     end
